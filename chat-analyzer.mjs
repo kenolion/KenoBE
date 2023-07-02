99115f0a260c641524b6c017b36581fb60ffd34a
Kenolion <kenolion13@gmail.com>
2023-07-02 22:44:28 +0800

    ChatAnalyzer will now total up the words and calculate
    to do - work on calCgePct method formula

--- chat-analyzer.mjs
+++ chat-analyzer.mjs
@@ -1,7 +1,12 @@
 import { readFileSync } from "fs";
 import { OUT_PATH, VID_STATS_NM } from "./app-const.mjs";
 import get from "lodash/get.js";
-
+/*
+  wordFreq = the amount of times the word occur
+  wordFreqDiff = the difference in wordFreq from pvsTime and curTime(positive means a greater increase in word frequency)
+  totalOcc = the total amount of times the list of words occur in the whole video
+  total = total messages in that timestamp
+*/
 export class ChatAnalyzer {
   constructor(words) {
     this.wordLis = words;
@@ -11,6 +16,7 @@ export class ChatAnalyzer {
     // stores the
     this.highestOccurenceObj = {};
     this.file = null;
+    // loaded from file {videoid}-stats.json, contains non totaled up data
     this.timeStampMap = null;
     this.timeStampObjTotal = {};
   }
@@ -39,24 +45,29 @@ export class ChatAnalyzer {
     for (let curTime in timeStampMap) {
       if (pvsTime != "") {
         this.wordFreqObj[curTime] = {};
+        this.timeStampObjTotal[curTime] = {};
+        this.wordFreqObj[curTime].totalOcc = 0;
         for (let tsKey in this.wordLis) {
           let word = this.wordLis[tsKey];
           let curWordsObj = timeStampMap[curTime].wordsObj;
           let pvsWordsObj = timeStampMap[pvsTime].wordsObj;
           curWordsObj[word] = curWordsObj[word] ?? 0;
           pvsWordsObj[word] = pvsWordsObj[word] ?? 0;
-
-          this.wordFreqObj[curTime][word] =
-            curWordsObj[word] - pvsWordsObj[word];
-          // change logic to total up the value
-          if (this.#isClippable(pvsTime, curTime, word)) {
-            //
-            console.log(curTime);
-            console.log(this.wordFreqObj[curTime][word]);
-            timeLis.push(curTime);
-          }
+          this.wordFreqObj[curTime].totalOcc += curWordsObj[word];
+        }
+        this.wordFreqObj[curTime].wordFreqDiff =
+          this.wordFreqObj[curTime].totalOcc -
+          get(this.wordFreqObj, `${pvsTime}.totalOcc`);
+        this.#calTimestampObjTotal(curTime, pvsTime);
+        // change logic to total up the value
+        if (this.#isClippable(pvsTime, curTime)) {
+          //
+          // console.log(curTime);
+          // console.log(this.wordFreqObj[curTime]);
+          timeLis.push(curTime);
         }
       }
+
       pvsTime = curTime;
     }
     console.log(timeLis);
@@ -67,7 +78,7 @@ export class ChatAnalyzer {
   // calculates the ratio of the word to the total number of words in the chat
   #calRatio(totMsg, wordTot) {
     let rt = (wordTot / totMsg) * 100;
-    //console.log("calRatio:", rt);
+    console.log("calRatio:", rt);
 
     return rt;
   }
@@ -80,26 +91,37 @@ export class ChatAnalyzer {
     //1687260840000000 see this, the previous change should also take into account how much ratio it was
     let rt = 0;
     if (prevAmt > 0) {
-      rt = (curDifTot / (prevAmt - 10) / 10) * 20;
-      //  console.log("cgePct:", rt);
+      rt = (2 * curDifTot) / (-10 + prevAmt);
+      //rt = (curDifTot / (prevAmt - 10) / 10) * 20;
+      console.log(`(${curDifTot} / (${prevAmt} - 10) / 10) * 20`);
+      console.log("cgePct:", rt);
       //rt = max([0, rt]);
     }
 
     return rt;
   }
 
-  #isClippable(pvsTime, curTime, word) {
+  #calTimestampObjTotal(curTime, pvsTime) {
+    this.timeStampObjTotal[curTime].ratio = this.#calRatio(
+      this.timeStampMap[curTime].total,
+      this.wordFreqObj[curTime].totalOcc
+    );
+    this.timeStampObjTotal[curTime].cgePct = this.#calCgePct(
+      get(this.wordFreqObj, `${curTime}.wordFreqDiff`),
+      get(this.wordFreqObj, `${pvsTime}.wordFreqDiff`)
+    );
+  }
+
+  // checks if the timestamp is clippable
+  #isClippable(pvsTime, curTime) {
     let score = 0;
-    let wordsObj = this.timeStampMap[curTime].wordsObj;
-    //    console.log("curTime:", curTime);
+    console.log("curTime:", curTime);
+
     score =
-      this.#calRatio(this.timeStampMap[curTime].total, wordsObj[word]) * 0.5 +
-      this.#calCgePct(
-        get(this.wordFreqObj, `${curTime}.${word}`),
-        get(this.timeStampMap, `${pvsTime}.wordsObj.${word}`)
-      ) *
-        0.5;
-    //    console.log("Score:", score, "\n");
-    return score > 10 ? true : false;
+      this.timeStampObjTotal[curTime].ratio * 0.5 +
+      this.timeStampObjTotal[curTime].cgePct * 0.5;
+    console.log("Score:", score, "\n");
+    console.log("---------\n");
+    return score > 25;
   }
 }
